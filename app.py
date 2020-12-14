cb4f1f149e4b1d0567e496d06463d0326d5c187e
karmashipley <74046863+karmashipley@users.noreply.github.com>
2020-12-14 12:55:23 +0900

    test

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+import requests
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
@@ -39,6 +42,17 @@ def list_dong():
     return jsonify({'result': 'success', 'addresses': addresses})
 #
 #
+url = 'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?serviceKey=97cuqDDHG2c0S%2Bia01naWZSkqK5hz8svWszWFQ7h8pdi%2BUgafcVl4O9six0eKcmLe7BF%2FXMaOkrC4h%2Fc5dcofQ%3D%3D&pageNo=1&numOfRows=10&LAWD_CD=11110&DEAL_YMD=201512'
+params = {'serviceKey':'CpiOdC5ys8I193uQraXgAjdPTK0cjePBKcxRATkQIFNdg%2BiFRetQcI0fSNyWFM7klpIw%2Bk9zSaB%2BuotTe9%2FZPQ%3D%3D','pageNo':1,'numOfRows':10,'LAWD_CD':'11110','DEAL_YMD':'202010'}
+res = requests.get(url, params=params)
+tree = ET.fromstring(res.content)
+items = tree.findall('body/items/item')
+for item in items:
+    print(item.find('법정동').text)
+    print(item.find('거래금액').text)
+    print(item.find('법정동읍면동코드').text)
+    print(item.find('아파트').text)
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
